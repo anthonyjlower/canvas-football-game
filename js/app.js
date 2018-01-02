cb0d6af8b04fbe1bc2f51d97964346bcec3563e7
Anthony <anthonyjlower@gmail.com>
2018-01-02 09:14:11 -0600

    Switched the opponent to a class so we can generate mulitple opponents at the same time

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 
 const canvas = document.getElementById('field');
 const ctx = canvas.getContext("2d");
-let round = 1;
-let score = 0;
-let turn = "Player 1";
+const round = 1;
+const score = 0;
+const turn = "Player 1";
 
 const runningBack = {
 	body: {},
@@ -43,53 +43,24 @@ const runningBack = {
 }
 
 
-// class Opponent {
-// 	constructor(number){
-// 		this.number = number;
-// 		this.speed = 10;
-// 		this.body = {};
-// 		this.direction = "";
-// 	}
-// 	initOpponent(){
-// 		this.body = {x: 200, y: 0, r: 12.5, e: 0}
-// 	}
-// 	drawBody() {
-// 		ctx.beginPath();
-// 		ctx.arc(this.body.x, this.body.y, this.body.r, this.body.e, Math.PI*2)
-// 		ctx.strokeStyle = 'red';
-// 		ctx.fill();
-// 		ctx.closePath();
-// 	}
-// };
-
-// const factory = {
-// 	roster: [],
-// 	createOpponent() {
-// 		for(let i = 1; i <= round + 2; i++){
-// 			const newPlayer = new Opponent(this.roster.length);
-// 			this.roster.push(newPlayer);
-// 		}
-// 	}
-// }
-
-
-
-
-const opponent = {
-		speed: 10,
-		body: {},
+class Opponent {
+	constructor(number){
+		this.number = number;
+		this.speed = 10;
+		this.body = {};
+		this.direction = "";
+	}
 	initOpponent(){
 		this.body = {x: 200, y: 0, r: 12.5, e: 0}
-	},
+	}
 	drawBody() {
 		ctx.beginPath();
 		ctx.arc(this.body.x, this.body.y, this.body.r, this.body.e, Math.PI*2)
-		ctx.strokeStyle = 'blue';
+		ctx.strokeStyle = 'black';
 		ctx.fill();
 		ctx.closePath();
-	},
-	move: function(){
-		
+	}
+	move() {
 		let randomNum = Math.floor(Math.random()*100)
 
 		// 50% of the time the opponenet moves down
@@ -111,6 +82,56 @@ const opponent = {
 		}	
 	}
 };
+
+const factory = {
+	roster: [],
+	createOpponent() {
+		for(let i = 1; i <= round + 2; i++){
+			const newPlayer = new Opponent(this.roster.length);
+			this.roster.push(newPlayer);
+		}
+	}
+}
+
+
+
+
+// const opponent = {
+// 		speed: 10,
+// 		body: {},
+// 	initOpponent(){
+// 		this.body = {x: 200, y: 0, r: 12.5, e: 0}
+// 	},
+// 	drawBody() {
+// 		ctx.beginPath();
+// 		ctx.arc(this.body.x, this.body.y, this.body.r, this.body.e, Math.PI*2)
+// 		ctx.strokeStyle = 'blue';
+// 		ctx.fill();
+// 		ctx.closePath();
+// 	},
+	// move: function(){
+		
+	// 	let randomNum = Math.floor(Math.random()*100)
+
+	// 	// 50% of the time the opponenet moves down
+	// 	if (randomNum <= 49) {
+	// 		this.body = {x: this.body.x, y: this.body.y + this.speed, r: 12.5, e:0}
+	// 		this.drawBody()
+	// 	// 20% of the time the opponent moves left
+	// 	} else if (randomNum >= 50 && randomNum <= 69){	
+	// 		this.body = {x: this.body.x - this.speed, y: this.body.y, r: 12.5, e:0}
+	// 		this.drawBody()
+	// 	// 20% of the time the opponent moves right
+	// 	} else if (randomNum >= 70 && randomNum <= 89){	
+	// 		this.body = {x: this.body.x + this.speed, y: this.body.y, r: 12.5, e:0}
+	// 		this.drawBody()
+	// 	//10% of the time the opponent move up
+	// 	} else {
+	// 		this.body = {x: this.body.x, y: this.body.y - this.speed, r: 12.5, e:0}
+	// 		this.drawBody()
+	// 	}	
+	// }
+// };
 
 
 
@@ -163,20 +184,19 @@ document.addEventListener('keydown', function(event){
 let animateCanvas = function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	runningBack.drawBody();
-	opponent.drawBody();
+	factory.roster[0].drawBody();
 	window.requestAnimationFrame(animateCanvas)
 }
 
 
 let intervalID = setInterval(()=>{
-		opponent.move()
-	}, 1000)
+		factory.roster[0].move()
+	}, 500)
 
 
+factory.createOpponent()
 runningBack.initHero();
-opponent.initOpponent();
-
-// factory.createOpponent()
+factory.roster[0].initOpponent();
 animateCanvas();
 
 
