8e813e93e61e2de5b7638f1f6b22c8ba6c72ba33
Anthony <anthonyjlower@gmail.com>
2018-01-03 11:49:21 -0600

    Adjusted starting point for the defense, changed the color for the defense, added prompt to player 2 player mode

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-// console.log("working")
-
+// Game Variables
 const canvas = document.getElementById('field');
 const ctx = canvas.getContext("2d");
 let round = 1;
 let player1Score = 0;
+let player2Score = 0;
 const turn = "Player 1";
+let player1IsAlive = false;
+let player2IsAlive = false;
 let timer;
-const fieldWidth = document.getElementById('field').width;
-const fieldHeight = document.getElementById('field').height;
 
+
+//Button and Modal selectors
 const playAgainBtn = document.getElementById('play-again');
 const tackledModal = document.getElementById('tackled-modal');
 const nextRoundBtn = document.getElementById("next-round");
@@ -17,70 +19,78 @@ const howToBtn = document.getElementById('how-to');
 const howToModel = document.getElementById('how-to-modal');
 const startGameBtn = document.getElementById('start-game');
 const closeBtn = document.getElementById('close');
+const startModel = document.getElementById('start-modal');
+const onePlayerBtn = document.getElementById('one-player');
+const twoPlayerBtn = document.getElementById('two-players');
 
 
+
+//User Controlled Players
 const runningBack = {
 	body: {},
 	direction: "",
-	speed: 10,
+	speed: 8,
+	size: 8,
 	initBody: function(){
-		this.body = {x: fieldWidth / 2, y: fieldHeight - this.speed, r: 12.5, e: 0}
+		this.body = {x: canvas.width / 2, y: canvas.height - this.speed, r: this.size, e: 0}
 	},
 	drawBody: function() {
 		ctx.beginPath();
 		ctx.arc(this.body.x, this.body.y, this.body.r, this.body.e, Math.PI*2)
-		ctx.strokeStyle = 'black';
+		ctx.fillStyle = "black";
 		ctx.fill();
 		ctx.closePath();
 	},
 	move: function(){
 		if (this.direction === 'right') {
-			if (this.body.x + this.speed < fieldWidth - 10) {
-				this.body = {x: this.body.x + this.speed, y: this.body.y, r: 12.5, e:0}
+			if (this.body.x + this.speed < canvas.width - 10) {
+				this.body = {x: this.body.x + this.speed, y: this.body.y, r: this.size, e:0}
 			}
 		} else if (this.direction === 'juke right'){
-			if (this.body.x + 30 < fieldWidth - 10) {
-				this.body = {x: this.body.x + 30, y: this.body.y, r: 12.5, e:0}
+			if (this.body.x + 30 < canvas.width - 10) {
+				this.body = {x: this.body.x + 30, y: this.body.y, r: this.size, e:0}
 			}
 		} else if (this.direction === 'left'){
 			if (this.body.x - this.speed > 10) {
-				this.body = {x: this.body.x - this.speed, y: this.body.y, r: 12.5, e:0}
+				this.body = {x: this.body.x - this.speed, y: this.body.y, r: this.size, e:0}
 			}
 		} else if (this.direction === 'juke left'){	
 			if (this.body.x - 30 > 30) {
-				this.body = {x: this.body.x - 30, y: this.body.y, r: 12.5, e:0}
+				this.body = {x: this.body.x - 30, y: this.body.y, r: this.size, e:0}
 			}
 		} else if (this.direction === 'up'){
 			if (this.body.y - this.speed > 0) {
-				this.body = {x: this.body.x, y: this.body.y -this.speed, r: 12.5, e:0}
+				this.body = {x: this.body.x, y: this.body.y -this.speed, r: this.size, e:0}
 			}
 		} else if (this.direction === 'down'){
-			if (this.body.y + this.speed < fieldHeight) {
-				this.body = {x: this.body.x, y: this.body.y +this.speed, r: 12.5, e:0}
+			if (this.body.y + this.speed < canvas.height) {
+				this.body = {x: this.body.x, y: this.body.y +this.speed, r: this.size, e:0}
 			}
 		} else if (this.direction === 'truck'){
 			if (this.body.y - 30 > 0) {
-				this.body = {x: this.body.x, y: this.body.y - 30, r: 12.5, e:0}
+				this.body = {x: this.body.x, y: this.body.y - 30, r: this.size, e:0}
 			}
 		}
 	}
 }
 
 
+// Computer controlled players
 class Opponent {
 	constructor(number){
 		this.number = number;
-		this.speed = 10;
+		this.speed = 8;
 		this.body = {};
+		this.size = 8;
 		this.direction = "";
 	}
 	initBody(){
-		this.body = {x: Math.floor(Math.random()*fieldWidth), y: this.speed, r: 12.5, e: 0}
+		this.body = {x: Math.floor(Math.random()*canvas.width), y: 142.2, r: this.size, e: 0}
 	}
 	drawBody() {
 		ctx.beginPath();
 		ctx.arc(this.body.x, this.body.y, this.body.r, this.body.e, Math.PI*2)
-		ctx.strokeStyle = 'black';
+		ctx.fillStyle = 'red';
 		ctx.fill();
 		ctx.closePath();
 	}
@@ -89,26 +99,26 @@ class Opponent {
 
 		// 50% of the time the opponenet moves down
 		if (randomNum <= 49) {
-			if (this.body.y + this.speed < fieldHeight - 10) {
-				this.body = {x: this.body.x, y: this.body.y + this.speed, r: 12.5, e:0}
+			if (this.body.y + this.speed < canvas.height - 10) {
+				this.body = {x: this.body.x, y: this.body.y + this.speed, r: this.size, e:0}
 				this.drawBody()
 			}
 		// 20% of the time the opponent moves left
 		} else if (randomNum >= 50 && randomNum <= 69){	
 			if (this.body.x - this.speed > 10) {
-				this.body = {x: this.body.x - this.speed, y: this.body.y, r: 12.5, e:0}
+				this.body = {x: this.body.x - this.speed, y: this.body.y, r: this.size, e:0}
 				this.drawBody()
 			}
 		// 20% of the time the opponent moves right
 		} else if (randomNum >= 70 && randomNum <= 89){
-			if (this.body.x + this.speed < fieldWidth - 10) {
-				this.body = {x: this.body.x + this.speed, y: this.body.y, r: 12.5, e:0}
+			if (this.body.x + this.speed < canvas.width - 10) {
+				this.body = {x: this.body.x + this.speed, y: this.body.y, r: this.size, e:0}
 				this.drawBody()
 			}
 		//10% of the time the opponent move up
 		} else {
 			if (this.body.x - this.speed > 10) {
-				this.body = {x: this.body.x, y: this.body.y - this.speed, r: 12.5, e:0}
+				this.body = {x: this.body.x, y: this.body.y - this.speed, r: this.size, e:0}
 				this.drawBody()
 			}
 		}	
@@ -135,40 +145,37 @@ const fieldLines = {
 	
 		// Outside lines
 		ctx.beginPath();
-		ctx.rect(0, 0, fieldWidth, fieldHeight);
+		ctx.rect(0, 0, canvas.width, canvas.height);
 		ctx.fillStyle = "green";
 		ctx.fill();
 		ctx.lineWidth = 10;
       	ctx.strokeStyle = "#FFF";
       	ctx.stroke();
       	ctx.closePath();
-      	ctx.fillStyle = "#FFF";
 
       	//Home Endzone
       	ctx.beginPath();
-      	ctx.rect(5, 5, fieldWidth - 10, 30)
+      	ctx.rect(5, 5, canvas.width - 10, 30)
       	ctx.fillStyle = "blue";
       	ctx.fill();
-      	ctx.lineWidth = 4;
+      	ctx.lineWidth = 2;
       	ctx.strokeStyle = "#FFF";
       	ctx.stroke();
       	ctx.closePath();
-      	ctx.fillStyle = "#FFF"
 
       	//Away Endzone
       	ctx.beginPath();
-      	ctx.rect(5, fieldHeight - 35, fieldWidth - 10, 30)
+      	ctx.rect(5, canvas.height - 35, canvas.width - 10, 30)
       	ctx.fillStyle = "blue";
       	ctx.fill();
-      	ctx.lineWidth = 4;
+      	ctx.lineWidth = 2;
       	ctx.strokeStyle = "#FFF";
       	ctx.stroke();
       	ctx.closePath();
-      	ctx.fillStyle = "#FFF"
 
       	//50 Yard Line
       	ctx.beginPath();
-      	ctx.rect(0, fieldHeight / 2, fieldWidth, 1)
+      	ctx.rect(0, canvas.height / 2, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -176,7 +183,7 @@ const fieldLines = {
 
       	//10 Yard LIne
 		ctx.beginPath();
-      	ctx.rect(0, 89.6, fieldWidth, 1)
+      	ctx.rect(0, 89.6, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -184,7 +191,7 @@ const fieldLines = {
 
       	//20 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -192,7 +199,7 @@ const fieldLines = {
 
       	//30 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 2, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 2, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -200,7 +207,7 @@ const fieldLines = {
 
       	//40 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 3, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 3, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -209,7 +216,7 @@ const fieldLines = {
 
       	//60 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 5, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 5, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -217,7 +224,7 @@ const fieldLines = {
 
       	//70 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 6, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 6, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -226,7 +233,7 @@ const fieldLines = {
 
       	//70 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 7, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 7, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -234,7 +241,7 @@ const fieldLines = {
 
       	//80 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 8, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 8, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -242,7 +249,7 @@ const fieldLines = {
 
       	//90 Yard Line
 		ctx.beginPath();
-      	ctx.rect(0, 89.6 + 52.6 * 9, fieldWidth, 1)
+      	ctx.rect(0, 89.6 + 52.6 * 9, canvas.width, 1)
       	ctx.fillStyle = "white";
       	ctx.fill();
       	ctx.stroke();
@@ -252,6 +259,8 @@ const fieldLines = {
 
 
 
+
+//User Movement event listeners
 document.addEventListener('keydown', function(event){
 	let key = event.which;
 
@@ -293,12 +302,15 @@ document.addEventListener('keydown', function(event){
 })
 
 
+// Modal and Button click functions
+
 // When the user clicks on <playAgainBtn> close the modal and start over
 playAgainBtn.onclick = function() {
     factory.roster = [];
     player1Score = 0;
     document.getElementById('player1Points').innerText = player1Score;
     round = 1;
+    document.getElementById('round-display').innerText = round;
     tackledModal.style.display = "none";
 }
 
@@ -312,7 +324,6 @@ nextRoundBtn.onclick = function() {
 	startGame();
 }
 
-
 howToBtn.onclick = function() {
 	howToModel.style.display = "block";	
 }
@@ -322,9 +333,25 @@ closeBtn.onclick = function() {
 }
 
 startGameBtn.onclick = function() {
-	startGame();	
+	startModel.style.display = "block";	
 }
 
+onePlayerBtn.onclick = function() {
+	player1IsAlive = true
+	startModel.style.display = "none";
+	startGame();
+}
+
+twoPlayerBtn.onclick = function() {
+	player1IsAlive = true
+	player2IsAlive = true
+	startModel.style.display = "none";	
+	startGame();
+}
+
+
+
+//Animcate the canvas
 let animateCanvas = function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -360,6 +387,7 @@ const placeOpponents = function() {
 	}
 };
 
+//Looks for a tackle
 const collisionDetection = function() {
 	let playerX = runningBack.body.x;
 	let playerY = runningBack.body.y;
@@ -371,17 +399,19 @@ const collisionDetection = function() {
 		let xDiff = Math.abs(playerX - oppX);
 		let yDiff = Math.abs(playerY - oppY);
 
-		if (xDiff <= 12.5 && yDiff <= 12.5) {
+		if (xDiff <= runningBack.size && yDiff <= runningBack.size) {
 			endGame()
 		}
 	}
 };
 
+//Stops the game - runs when collisions are detected
 const endGame = function() {
 	stopOpp()
 	tackledModal.style.display = "block";
 };
 
+//Stope ths game - runs when the user gets into the endzone
 const scoreTd = function() {
 	if (runningBack.body.y < 30) {
 		stopOpp();
@@ -389,11 +419,15 @@ const scoreTd = function() {
 	};
 }
 
+
+
+
+
+
+//Draws the lines on the field
 fieldLines.draw();
 
-
-
-
+//Starts the game - runs no button click
 const startGame = function() {
 	factory.createOpponent()
 	runningBack.initBody();
